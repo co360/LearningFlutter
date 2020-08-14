c93335370e5bd6247fdb13088e7af5e80ff58586
muriloargol0 <murilo.argolo@live.com>
2020-08-13 21:58:49 -0300

    my tic tac toe game

--- Exercises/05/TICTACTOE/lib/HomePage.dart
+++ Exercises/05/TICTACTOE/lib/HomePage.dart
@@ -44,7 +44,14 @@ class _HomePageState extends State<HomePage> {
   playGame(int index) {
     if (this.gameState[index] == "empty") {
       setState(() {
-        if (this.isCross) {}
+        if (this.isCross) {
+          this.gameState[index] = "cross";
+        } else {
+          this.gameState[index] = "circle";
+        }
+
+        this.isCross = !this.isCross;
+        this.checkWin();
       });
     }
   }
@@ -70,10 +77,133 @@ class _HomePageState extends State<HomePage> {
         break;
     }
   }
+
   //TODO: check for win logic
+  checkWin() {
+    if ((gameState[0] != 'empty') &&
+        (gameState[0] == gameState[1]) &&
+        (gameState[1] == gameState[2])) {
+      setState(() {
+        this.message = '${this.gameState[0]} Wins';
+      });
+    } else if ((gameState[3] != 'empty') &&
+        (gameState[3] == gameState[4]) &&
+        (gameState[4] == gameState[5])) {
+      setState(() {
+        this.message = '${this.gameState[3]} Wins';
+      });
+    } else if ((gameState[6] != 'empty') &&
+        (gameState[6] == gameState[7]) &&
+        (gameState[7] == gameState[8])) {
+      setState(() {
+        this.message = '${this.gameState[6]} Wins';
+      });
+    } else if ((gameState[0] != 'empty') &&
+        (gameState[0] == gameState[3]) &&
+        (gameState[3] == gameState[6])) {
+      setState(() {
+        this.message = '${this.gameState[0]} Wins';
+      });
+    } else if ((gameState[1] != 'empty') &&
+        (gameState[1] == gameState[4]) &&
+        (gameState[4] == gameState[7])) {
+      setState(() {
+        this.message = '${this.gameState[1]} Wins';
+      });
+    } else if ((gameState[2] != 'empty') &&
+        (gameState[2] == gameState[5]) &&
+        (gameState[5] == gameState[8])) {
+      setState(() {
+        this.message = '${this.gameState[2]} Wins';
+      });
+    } else if ((gameState[0] != 'empty') &&
+        (gameState[0] == gameState[4]) &&
+        (gameState[4] == gameState[8])) {
+      setState(() {
+        this.message = '${this.gameState[0]} Wins';
+      });
+    } else if ((gameState[2] != 'empty') &&
+        (gameState[2] == gameState[4]) &&
+        (gameState[4] == gameState[6])) {
+      setState(() {
+        this.message = '${this.gameState[2]} Wins';
+      });
+    } else if (!gameState.contains('empty')) {
+      setState(() {
+        this.message = 'Game Draw';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TicTacToe Game'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: GridView.builder(
+              padding: EdgeInsets.all(20.0),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                childAspectRatio: 1.0,
+                crossAxisSpacing: 10.0,
+                mainAxisSpacing: 10.0,
+              ),
+              itemCount: this.gameState.length,
+              itemBuilder: (context, i) => SizedBox(
+                width: 100.0,
+                height: 100.0,
+                child: MaterialButton(
+                  onPressed: () {
+                    this.playGame(i);
+                  },
+                  child: Image(
+                    image: this.getImage(this.gameState[i]),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: Text(
+              this.message,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          MaterialButton(
+            padding: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 20.0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            color: Colors.purple,
+            minWidth: 300.0,
+            height: 50.0,
+            child: Text(
+              "Reset Game",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+              ),
+            ),
+            onPressed: () {
+              this.resetGame();
+            },
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: Text("Murilo Argolo - Learning Flutter"),
+          ),
+        ],
+      ),
+    );
   }
 }
